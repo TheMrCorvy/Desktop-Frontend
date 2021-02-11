966a73ce44956bac0eaaeca00be86433dc65a2bb
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-11 15:29:34 -0300

    moved the logic for translations to navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { useState, MouseEvent } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setTheme } from "../../redux/actions/themeActions"
 import { RootState } from "../../redux/store"
 
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core"
+import { AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem } from "@material-ui/core"
 
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 
 import Brightness4Icon from "@material-ui/icons/Brightness4"
 import NightsStayIcon from "@material-ui/icons/NightsStay"
+import TranslateIcon from "@material-ui/icons/Translate"
+
+import { translate } from "../../lang"
+import { setLanguage } from "../../redux/actions/langActions"
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -28,9 +32,25 @@ const useStyles = makeStyles((theme: Theme) =>
 const Navbar = () => {
 	const { theme } = useSelector((state: RootState) => state.theme)
 
+	const { language } = useSelector((state: RootState) => state.language)
+
 	const dispatch = useDispatch()
 
 	const classes = useStyles()
+
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleClose = (value: string) => {
+		setAnchorEl(null)
+
+		if (value !== "nothing") {
+			dispatch(setLanguage(value))
+		}
+	}
 
 	const toggleDarkTheme = () => {
 		if (theme === "dark") {
@@ -41,20 +61,41 @@ const Navbar = () => {
 	}
 
 	return (
-		<div className={classes.root}>
-			<AppBar position="fixed" color={theme === "dark" ? "inherit" : "secondary"}>
-				<Toolbar>
-					<Typography variant="h6" className={classes.title}>
-						News
-					</Typography>
+		<>
+			<div className={classes.root}>
+				<AppBar position="fixed" color={theme === "dark" ? "inherit" : "secondary"}>
+					<Toolbar>
+						<Typography variant="h6" className={classes.title}>
+							{translate("prueba", language)}
+						</Typography>
 
-					<Button color="inherit">Login</Button>
-					<IconButton edge="end" color="inherit" onClick={toggleDarkTheme}>
-						{theme === "dark" ? <NightsStayIcon /> : <Brightness4Icon />}
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-		</div>
+						<IconButton
+							aria-controls="lang-menu"
+							aria-haspopup="true"
+							color="inherit"
+							onClick={handleClick}
+						>
+							<TranslateIcon />
+						</IconButton>
+						<Button color="inherit">Login</Button>
+						<IconButton edge="end" color="inherit" onClick={toggleDarkTheme}>
+							{theme === "dark" ? <NightsStayIcon /> : <Brightness4Icon />}
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+			</div>
+			<Menu
+				id="lang-menu"
+				anchorEl={anchorEl}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={() => handleClose("nothing")}
+			>
+				<MenuItem onClick={() => handleClose("en")}>EN</MenuItem>
+				<MenuItem onClick={() => handleClose("es")}>ES</MenuItem>
+				<MenuItem onClick={() => handleClose("jp")}>JP</MenuItem>
+			</Menu>
+		</>
 	)
 }
 
