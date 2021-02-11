8e6c2a8edd008e032ccc7ce73b798f937547242b
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-11 14:27:03 -0300

    created landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react"
+
+const Landing: FC = () => {
+	return (
+		<div>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates eligendi ipsam
+			sint, reprehenderit deleniti dignissimos, possimus minima dolor cupiditate iure
+			molestias provident architecto incidunt enim eius ipsum itaque illum quam.
+		</div>
+	)
+}
+
+export default Landing
