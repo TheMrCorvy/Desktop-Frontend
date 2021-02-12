1d0b1b0c9787a429220d9042f50979c6a48de6ec
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-12 11:41:55 -0300

    added the install web app button

--- src/components/NavbarComponents/Drawer/index.tsx
+++ src/components/NavbarComponents/Drawer/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React, { FC, useEffect, useState } from "react"
 
 import {
 	SwipeableDrawer,
@@ -10,16 +10,50 @@ import {
 } from "@material-ui/core"
 
 import InboxIcon from "@material-ui/icons/MoveToInbox"
-import MailIcon from "@material-ui/icons/Mail"
 
 import { useSelector, useDispatch } from "react-redux"
 import { RootState } from "../../../redux/store"
 import { toggleDrawer } from "../../../redux/actions/drawerActions"
 
+let deferredPrompt: any
+
 const Drawer: FC = () => {
 	const { open } = useSelector((state: RootState) => state.open)
 
 	const dispatch = useDispatch()
+
+	const [installable, setInstallable] = useState(false)
+
+	useEffect(() => {
+		window.addEventListener("beforeinstallprompt", (e: Event) => {
+			// Prevent the mini-infobar from appearing on mobile
+			e.preventDefault()
+			// Stash the event so it can be triggered later.
+			deferredPrompt = e
+			// Update UI notify the user they can install the PWA
+			setInstallable(true)
+		})
+
+		window.addEventListener("appinstalled", () => {
+			// Log install to analytics
+			console.log("INSTALL: Success")
+		})
+	}, [])
+
+	const handleInstallClick = () => {
+		// Hide the app provided install promotion
+		setInstallable(false)
+		// Show the install prompt
+		deferredPrompt.prompt()
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult: any) => {
+			if (choiceResult.outcome === "accepted") {
+				console.log("User accepted the install prompt")
+			} else {
+				console.log("User dismissed the install prompt")
+			}
+		})
+	}
 
 	return (
 		<SwipeableDrawer
@@ -34,17 +68,23 @@ const Drawer: FC = () => {
 				onKeyDown={() => dispatch(toggleDrawer(false))}
 			>
 				<List>
-					{["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-						<>
-							<ListItem button key={text}>
-								<ListItemIcon>
-									{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-								</ListItemIcon>
-								<ListItemText primary={text} />
-							</ListItem>
-							<Divider />
-						</>
-					))}
+					{installable && (
+						<ListItem button onClick={() => handleInstallClick}>
+							<ListItemIcon>
+								<InboxIcon />
+							</ListItemIcon>
+							<ListItemText primary="install web app" />
+						</ListItem>
+					)}
+					{!installable && (
+						<ListItem button>
+							<ListItemIcon>
+								<InboxIcon />
+							</ListItemIcon>
+							<ListItemText primary="app is not installable" />
+						</ListItem>
+					)}
+					<Divider />
 				</List>
 			</div>
 		</SwipeableDrawer>
