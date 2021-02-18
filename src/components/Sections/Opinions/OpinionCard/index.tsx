fefabc36a4521ee3560c48b0b85fc06981fea388
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-18 15:00:29 -0300

    created a generic "feedback" sections component

---
+++ src/components/Sections/Opinions/OpinionCard/index.tsx
@@ -0,0 +1,76 @@
+import Recat from "react"
+
+import { Card, CardHeader, CardContent, CardActions, Avatar, Typography } from "@material-ui/core"
+
+import { Rating } from "@material-ui/lab"
+
+import { red } from "@material-ui/core/colors"
+
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles"
+
+export type OpinionCardT = {
+	user: {
+		firstName: string
+		lastName: string
+	}
+	date: string
+	opinion: {
+		body: string
+		isRating: boolean
+		rating?: number | null
+	}
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+			background: theme.palette.type === "dark" ? "rgba(0, 0, 0, 0.26)" : "#f5f5f5",
+		},
+		avatar: {
+			backgroundColor: red[500],
+			color: "white",
+		},
+		container: {
+			paddingTop: "3rem",
+			paddingBottom: "3rem",
+		},
+		textCenter: {
+			textAlign: "center",
+		},
+		divider: {
+			marginBottom: "5rem",
+		},
+	})
+)
+
+const OpinionCard = ({ user, date, opinion }: OpinionCardT) => {
+	const classes = useStyles()
+
+	const avatar = user.firstName.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase()
+
+	return (
+		<Card>
+			<CardHeader
+				avatar={
+					<Avatar aria-label="recipe" className={classes.avatar}>
+						{avatar}
+					</Avatar>
+				}
+				title={user.lastName + " " + user.firstName}
+				subheader={date}
+			/>
+			<CardContent>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{opinion.body}
+				</Typography>
+			</CardContent>
+			{opinion.isRating && (
+				<CardActions disableSpacing>
+					<Rating name="read-only" value={opinion.rating} precision={0.5} readOnly />
+				</CardActions>
+			)}
+		</Card>
+	)
+}
+export default OpinionCard
---
+++ src/components/Sections/Opinions/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from "react"
+
+import { Container, Typography, Grid } from "@material-ui/core"
+
+import { red } from "@material-ui/core/colors"
+
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles"
+
+import OpinionCard, { OpinionCardT } from "./OpinionCard"
+
+type Props = {
+	title: string
+	opinions: OpinionCardT[]
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+			background: theme.palette.type === "dark" ? "rgba(0, 0, 0, 0.26)" : "#f5f5f5",
+		},
+		avatar: {
+			backgroundColor: red[500],
+		},
+		container: {
+			paddingTop: "3rem",
+			paddingBottom: "3rem",
+		},
+		textCenter: {
+			textAlign: "center",
+		},
+		divider: {
+			marginBottom: "5rem",
+		},
+	})
+)
+
+const Opinions = ({ title, opinions }: Props) => {
+	const classes = useStyles()
+
+	return (
+		<div className={classes.root}>
+			<Container maxWidth="xl" className={classes.container}>
+				<Grid container spacing={3} justify="space-around">
+					<Grid item xs={12} className={classes.textCenter}>
+						<Typography variant="h4" gutterBottom>
+							{title}
+						</Typography>
+					</Grid>
+					{opinions.map((opinion, index) => (
+						<Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+							<OpinionCard {...opinion} />
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</div>
+	)
+}
+
+export default Opinions
