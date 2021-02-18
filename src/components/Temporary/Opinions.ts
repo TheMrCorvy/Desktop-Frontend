bce65c12b627a66446b36696b9d1c9410baadfa9
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-18 14:59:21 -0300

    created a temporary folder to store hardcoded data

---
+++ src/components/Temporary/Opinions.ts
@@ -0,0 +1,132 @@
+import { OpinionCardT } from "../Sections/Opinions/OpinionCard"
+
+export const suggestions: OpinionCardT[] = [
+	{
+		user: {
+			firstName: "Name 1",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: false,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 2",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: false,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 3",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: false,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 4",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: false,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 5",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: false,
+		},
+	},
+]
+
+export const ratings: OpinionCardT[] = [
+	{
+		user: {
+			firstName: "Name 1",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: true,
+			rating: 2.5,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 2",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: true,
+			rating: 2.5,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 3",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: true,
+			rating: 2.5,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 4",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: true,
+			rating: 2.5,
+		},
+	},
+	{
+		user: {
+			firstName: "Name 5",
+			lastName: "Example",
+		},
+		date: "September 14, 2016",
+		opinion: {
+			body:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem culpa optio animi deleniti ipsum dolorum voluptatem vel iste saepe ad sed maiores cupiditate, debitis nihil atque facilis minus! Alias, voluptatum.",
+			isRating: true,
+			rating: 2.5,
+		},
+	},
+]
