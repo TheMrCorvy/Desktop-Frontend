3df5a3b5bb8bb7b2ba81ed48e4fd7a91632b9d6b
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-19 15:04:52 -0300

    added the test for 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -8,9 +8,9 @@ import { makeStyles } from "@material-ui/core/styles"
 import HomeIcon from "@material-ui/icons/Home"
 
 import { useSelector } from "react-redux"
-import { RootState } from "../redux/store"
+import { RootState } from "../../redux/store"
 
-import { translate } from "../lang"
+import { translate } from "../../lang"
 
 const useStyles = makeStyles({
 	container: {
@@ -33,7 +33,7 @@ const NotFound: FC = () => {
 	const classes = useStyles()
 
 	return (
-		<Container maxWidth="xl" className={classes.container}>
+		<Container maxWidth="xl" className={classes.container} data-testid="test_not_found_page">
 			<Grid container justify="center" className={classes.centerAll} spacing={0}>
 				<Grid item xs={12}>
 					<Typography variant="h3" paragraph gutterBottom>
