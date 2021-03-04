b9554f2619c05188c75ee3809837b3b4a93ddab2
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-04 15:11:56 -0300

    added the validation to emailCode form component

--- src/components/Sections/LoginOptions/EmailCode/index.tsx
+++ src/components/Sections/LoginOptions/EmailCode/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import {
 	Box,
 	Grid,
@@ -7,8 +7,8 @@ import {
 	OutlinedInput,
 	InputLabel,
 	FormControl,
-	TextField,
 	Input,
+	Typography,
 } from "@material-ui/core"
 
 import TimerButton from "../../../TimerButton"
@@ -16,6 +16,13 @@ import TimerButton from "../../../TimerButton"
 import { useSelector } from "react-redux"
 import { RootState } from "../../../../redux/store"
 import { translate } from "../../../../lang"
+
+import { useForm } from "react-hook-form"
+
+type FormInputs = {
+	mainEmail: string
+	recoveryEmail: string
+}
 
 type Props = {
 	isRecovery: boolean
@@ -28,14 +35,28 @@ const EmailCode = ({ testing, isRobot, isRecovery }: Props) => {
 
 	const [canSubmit, setCanSubmit] = useState(false)
 
-	const handleClick = () => {
-		setCanSubmit(true)
+	const { register, errors, handleSubmit } = useForm()
 
-		if (testing) {
-			console.log("hola mundo")
-		} else {
-			console.log("production api call")
-		}
+	const requiredMessage = translate("form_validation_messages", lng, 0)
+	const maxCharMessage = translate("form_validation_messages", lng, 1)
+	const minCharMessage = translate("form_validation_messages", lng, 2)
+	const emailPattern = {
+		value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+		message: translate("form_validation_messages", lng, 3),
+	}
+
+	const onSubmit = (data: FormInputs) => {
+		if (testing) return
+
+		console.log("production api call")
+		console.log(data)
+	}
+
+	const sendEmail = () => {
+		if (testing) return
+
+		setCanSubmit(true)
+		console.log("sending email...")
 	}
 
 	return (
@@ -45,58 +66,86 @@ const EmailCode = ({ testing, isRobot, isRecovery }: Props) => {
 		>
 			<Grid container justify="center" spacing={3}>
 				<Grid item xs={12} sm={6}>
-					<Grid container justify="center" spacing={3}>
+					<Grid
+						container
+						justify="center"
+						spacing={3}
+						component="form"
+						onSubmit={handleSubmit(onSubmit)}
+					>
 						{isRecovery && (
 							<Grid item xs={12}>
 								<FormControl variant="outlined" fullWidth>
-									<InputLabel htmlFor="outlined-adornment-password">
-										{translate("auth_form_texts", lng, 2)}
-									</InputLabel>
+									<InputLabel>{translate("auth_form_texts", lng, 2)}</InputLabel>
 									<OutlinedInput
 										label={translate("auth_form_texts", lng, 2)}
-										inputProps={{ "data-testid": "test_main_email" }}
+										name="mainEmail"
+										required
+										type="email"
+										inputProps={{
+											"data-testid": "test_main_email",
+											ref: register({
+												required: {
+													value: true,
+													message: requiredMessage,
+												},
+												maxLength: {
+													value: 50,
+													message: maxCharMessage,
+												},
+												minLength: {
+													value: 5,
+													message: minCharMessage,
+												},
+												pattern: emailPattern,
+											}),
+										}}
+										error={errors?.mainEmail ? true : false}
 									/>
+									{errors.mainEmail && (
+										<Typography variant="body2">
+											{errors.mainEmail.message}
+										</Typography>
+									)}
 								</FormControl>
 							</Grid>
 						)}
 						<Grid item xs={12}>
 							<FormControl variant="outlined" fullWidth>
-								<InputLabel htmlFor="outlined-adornment-password">
+								<InputLabel>
 									{isRecovery
 										? translate("auth_form_texts", lng, 3)
 										: translate("auth_form_texts", lng, 0)}
 								</InputLabel>
-								{/* <TextField
+								<OutlinedInput
 									label={
 										isRecovery
 											? translate("auth_form_texts", lng, 3)
 											: translate("auth_form_texts", lng, 0)
 									}
-									variant="outlined"
+									name="recoveryEmail"
+									required
+									type="email"
 									inputProps={{
-										"data-testid": "test_email_to_send_code",
-										endAdornment: (
-											<InputAdornment position="end" onClick={handleClick}>
-												{isRobot ? (
-													<Button
-														size="small"
-														disabled
-														variant="contained"
-													>
-														{translate("send_email", lng)}
-													</Button>
-												) : (
-													<TimerButton
-														title={translate("send_email", lng)}
-													/>
-												)}
-											</InputAdornment>
-										),
+										ref: register({
+											required: {
+												value: true,
+												message: requiredMessage,
+											},
+											maxLength: {
+												value: 50,
+												message: maxCharMessage,
+											},
+											minLength: {
+												value: 5,
+												message: minCharMessage,
+											},
+											pattern: emailPattern,
+										}),
 									}}
-								/> */}
-								<Input
+									error={errors?.recoveryEmail ? true : false}
 									endAdornment={
-										<InputAdornment position="end" onClick={handleClick}>
+										<InputAdornment position="end" onClick={sendEmail}>
 											{isRobot ? (
 												<Button size="small" disabled variant="contained">
 													{translate("send_email", lng)}
@@ -107,17 +156,56 @@ const EmailCode = ({ testing, isRobot, isRecovery }: Props) => {
 										</InputAdornment>
 									}
 								/>
+								{errors.recoveryEmail && (
+									<Typography variant="body2">
+										{errors.recoveryEmail.message}
+									</Typography>
+								)}
 							</FormControl>
 						</Grid>
 						<Grid item xs={12}>
 							<FormControl variant="outlined" fullWidth>
-								<InputLabel htmlFor="outlined-adornment-password">
-									{translate("auth_form_texts", lng, 1)}
-								</InputLabel>
+								<InputLabel>{translate("auth_form_texts", lng, 1)}</InputLabel>
 								<OutlinedInput
 									label={translate("auth_form_texts", lng, 1)}
-									inputProps={{ "data-testid": "test_verification_code" }}
+									name="recoveryEmail"
+									required
+									type="email"
+									inputProps={{
+										"data-testid": "test_verification_code",
+										ref: register({
+											required: {
+												value: true,
+												message: requiredMessage,
+											},
+											maxLength: {
+												value: 6,
+												message: maxCharMessage,
+											},
+											minLength: {
+												value: 6,
+												message: minCharMessage,
+											},
+										}),
+									}}
+									error={errors?.recoveryEmail ? true : false}
+									endAdornment={
+										<InputAdornment position="end" onClick={sendEmail}>
+											{isRobot ? (
+												<Button size="small" disabled variant="contained">
+													{translate("send_email", lng)}
+												</Button>
+											) : (
+												<TimerButton title={translate("send_email", lng)} />
+											)}
+										</InputAdornment>
+									}
 								/>
+								{errors.recoveryEmail && (
+									<Typography variant="body2">
+										{errors.recoveryEmail.message}
+									</Typography>
+								)}
 							</FormControl>
 						</Grid>
 						<Grid item xs={12}>
@@ -127,6 +215,7 @@ const EmailCode = ({ testing, isRobot, isRecovery }: Props) => {
 								fullWidth
 								disableElevation
 								disabled={!canSubmit}
+								onSubmit={handleSubmit(onSubmit)}
 							>
 								{translate("navbar_login_btn", lng)}
 							</Button>
