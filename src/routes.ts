132cc418dd6d33d5c4298a45f44e013c9d1ee78d
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-08 11:00:49 -0300

    added the downloads page to routes.ts & removed the property "name"

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,9 @@ import { lazy, LazyExoticComponent } from "react"
 const Landing = lazy(() => import("./pages/Landing"))
 const Login = lazy(() => import("./pages/Login"))
 const Register = lazy(() => import("./pages/Register"))
+const DownloadsPage = lazy(() => import("./pages/DownloadsPage"))
 
 export type RouteType = {
-	name: string
 	component: LazyExoticComponent<any>
 	requiresAuth: boolean
 	path: string
@@ -14,23 +14,26 @@ export type RouteType = {
 
 const routes: RouteType[] = [
 	{
-		name: "Landing Page",
 		component: Landing,
 		requiresAuth: false,
 		path: "/",
 	},
 	{
-		name: "Login Page",
 		component: Login,
 		requiresAuth: false,
 		path: "/login",
 		guestOnly: true,
 	},
 	{
-		name: "Register Page",
 		component: Register,
 		requiresAuth: false,
 		path: "/register",
+		guestOnly: true,
+	},
+	{
+		component: DownloadsPage,
+		requiresAuth: false,
+		path: "/downloads",
 		guestOnly: true,
 	},
 ]
