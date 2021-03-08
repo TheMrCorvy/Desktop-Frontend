4ff5a447b8a85fa974eecd7844143d30e3080f1f
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-08 11:00:59 -0300

    created downloads page

---
+++ src/pages/DownloadsPage.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Downloads from "../components/Sections/Downloads"
+
+const DownloadsPage = () => {
+	return (
+		<div
+			style={{
+				minHeight: "30vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Downloads />
+		</div>
+	)
+}
+
+export default DownloadsPage
