26dfa6a3f0148caf2c0cdcc6d95c19965f437b01
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-08 18:37:16 -0300

    the downloads page is officially complete

--- src/pages/DownloadsPage.tsx
+++ src/pages/DownloadsPage.tsx
@@ -20,12 +20,25 @@ import Downloads from "../components/Sections/Downloads"
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		divider: {
-			marginTop: 10,
-			marginBottom: 10,
+			marginBottom: "5rem",
+			marginTop: "5rem",
 		},
 		recommendedLinks: {
-			marginRight: 15,
+			marginRight: 35,
 			color: theme.palette.primary.main,
+		},
+		marginBottom: {
+			marginBottom: "3rem",
+		},
+		landingSection: {
+			minHeight: "70vh",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		container: {
+			paddingBottom: "5rem",
+			paddingTop: "2rem",
 		},
 	})
 )
@@ -40,21 +53,12 @@ const DownloadsPage = () => {
 	const otherApps: RecommendedAppsType[] = recommendedApps(lng)
 
 	return (
-		<Container maxWidth="xl" style={{ paddingBottom: "5rem", paddingTop: "2rem" }}>
+		<Container maxWidth="xl" className={classes.container}>
 			<Grid container justify="center" spacing={3}>
-				<Grid
-					item
-					xs={12}
-					style={{
-						minHeight: "70vh",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-					}}
-				>
+				<Grid item xs={12} className={classes.landingSection}>
 					<Downloads testing alternative />
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<Hidden xsDown>
 						<Typography variant="h3" paragraph gutterBottom>
 							{translate("downloads_page_titles", lng, 0)}
@@ -66,7 +70,7 @@ const DownloadsPage = () => {
 						</Typography>
 					</Hidden>
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<Typography variant="body1" paragraph gutterBottom>
 						{translate("downloads_page_texts", lng, 0)}
 					</Typography>
@@ -77,10 +81,10 @@ const DownloadsPage = () => {
 						{translate("downloads_page_texts", lng, 2)}
 					</Typography>
 				</Grid>
-				<Grid xs={12} sm={8}>
-					<Divider style={{ marginBottom: "5rem", marginTop: "5rem" }} />
+				<Grid item xs={12} sm={11} md={8}>
+					<Divider className={classes.divider} />
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<Typography variant="h4" paragraph gutterBottom>
 						{translate("downloads_page_titles", lng, 1)}
 					</Typography>
@@ -94,47 +98,55 @@ const DownloadsPage = () => {
 						{translate("downloads_page_texts", lng, 5)}
 					</Typography>
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<ul>
 						{otherApps.map((app, index) => (
-							<li style={{ marginBottom: 32 }} key={index}>
+							<li className={classes.marginBottom} key={index}>
 								<Typography variant="h5" paragraph gutterBottom>
 									{app.appName}
 								</Typography>
 								<Typography variant="body1" paragraph gutterBottom>
 									{app.bodyText}
 								</Typography>
-								<Link
-									className={classes.recommendedLinks}
-									href={app.linkPlayStore}
-									target="_blank"
-								>
-									Google PlayStore
-								</Link>
-								<Link
-									className={classes.recommendedLinks}
-									href={app.linkAppleStore}
-									target="_blank"
-								>
-									Apple AppStore
-								</Link>
-								{app.linkOfficialPage && (
-									<Link
-										className={classes.recommendedLinks}
-										href={app.linkOfficialPage}
-										target="_blank"
-									>
-										{translate("official_site", lng)}
-									</Link>
-								)}
+								<Grid container spacing={2}>
+									<Grid item xs={12} sm={4}>
+										<Link
+											className={classes.recommendedLinks}
+											href={app.linkPlayStore}
+											target="_blank"
+										>
+											Google PlayStore
+										</Link>
+									</Grid>
+									<Grid item xs={12} sm={4}>
+										<Link
+											className={classes.recommendedLinks}
+											href={app.linkAppleStore}
+											target="_blank"
+										>
+											Apple AppStore
+										</Link>
+									</Grid>
+									<Grid item xs={12} sm={4}>
+										{app.linkOfficialPage && (
+											<Link
+												className={classes.recommendedLinks}
+												href={app.linkOfficialPage}
+												target="_blank"
+											>
+												{translate("official_site", lng)}
+											</Link>
+										)}
+									</Grid>
+								</Grid>
 							</li>
 						))}
 					</ul>
 				</Grid>
-				<Grid xs={12} sm={8}>
-					<Divider style={{ marginBottom: "5rem", marginTop: "5rem" }} />
+				<Grid item xs={12} sm={11} md={8}>
+					<Divider className={classes.divider} />
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<Typography variant="h4" paragraph gutterBottom>
 						{translate("downloads_page_titles", lng, 2)}
 					</Typography>
@@ -151,7 +163,7 @@ const DownloadsPage = () => {
 						{translate("downloads_page_texts", lng, 9)}
 					</Typography>
 				</Grid>
-				<Grid item xs={12} sm={8}>
+				<Grid item xs={12} sm={11} md={8}>
 					<ul>
 						{recommendedTwoFA.map((app, index) => (
 							<li style={{ marginBottom: 32 }} key={index}>
@@ -161,20 +173,37 @@ const DownloadsPage = () => {
 								<Typography variant="body1" paragraph gutterBottom>
 									{app.bodyText}
 								</Typography>
-								<Link
-									className={classes.recommendedLinks}
-									href={app.linkPlayStore}
-									target="_blank"
-								>
-									Google PlayStore
-								</Link>
-								<Link
-									className={classes.recommendedLinks}
-									href={app.linkAppleStore}
-									target="_blank"
-								>
-									Apple AppStore
-								</Link>
+								<Grid container spacing={2}>
+									<Grid item xs={12} sm={4}>
+										<Link
+											className={classes.recommendedLinks}
+											href={app.linkPlayStore}
+											target="_blank"
+										>
+											Google PlayStore
+										</Link>
+									</Grid>
+									<Grid item xs={12} sm={4}>
+										<Link
+											className={classes.recommendedLinks}
+											href={app.linkAppleStore}
+											target="_blank"
+										>
+											Apple AppStore
+										</Link>
+									</Grid>
+									<Grid item xs={12} sm={4}>
+										{app.linkOfficialPage && (
+											<Link
+												className={classes.recommendedLinks}
+												href={app.linkOfficialPage}
+												target="_blank"
+											>
+												{translate("official_site", lng)}
+											</Link>
+										)}
+									</Grid>
+								</Grid>
 							</li>
 						))}
 					</ul>
