849216a3d755550dc3637c6d58f1914bb7bb652f
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-15 11:52:07 -0300

    now the user wont be stuck on err 500 page

--- src/components/ScrollTop.ts
+++ src/components/ScrollTop.ts
@@ -1,11 +1,23 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../redux/store"
+import { clearError } from "../redux/actions/errorHandlingActions"
+
 export default function ScrollTop() {
+	const { err } = useSelector((state: RootState) => state.err)
+
+	const dispatch = useDispatch()
+
 	const { pathname } = useLocation()
 
 	useEffect(() => {
 		window.scrollTo(0, 0)
+
+		if (err) {
+			dispatch(clearError())
+		}
 	}, [pathname])
 
 	return null
