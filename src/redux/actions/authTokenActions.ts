c48c969dfe900b99925d62a53ddcd1698f2a1547
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-15 10:52:17 -0300

    now it drops the indexedDB on logout

--- src/redux/actions/authTokenActions.ts
+++ src/redux/actions/authTokenActions.ts
@@ -1,5 +1,7 @@
 import { AuthAction, LOG_IN, LOG_OUT } from "../types"
 
+import { dropDatabase } from "../../misc/indexedDB"
+
 export const login = (token: string): AuthAction => {
 	//we won't be setting the token on the local storage or anything
 	//it'll be stored just on the memory, and if the user refreshes the page, the session will be closed
@@ -10,6 +12,8 @@ export const login = (token: string): AuthAction => {
 }
 
 export const logOut = (): AuthAction => {
+	dropDatabase()
+
 	return {
 		type: LOG_OUT,
 		payload: null,
