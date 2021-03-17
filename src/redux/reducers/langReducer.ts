461e099ea247f6b69c15495b6f1a76263fb15a1d
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-17 16:04:06 -0300

    small changes to the initial state of langReducer

--- src/redux/reducers/langReducer.ts
+++ src/redux/reducers/langReducer.ts
@@ -2,9 +2,19 @@ import { SET_LANGUAGE, LangAction, LangStateI } from "../types"
 
 const localStorageLang = localStorage.getItem("preferred_language")
 
-const initialState: LangStateI = {
-	lng: localStorageLang ? localStorageLang : "en",
+const navLang = navigator.language.substring(0, 2)
+
+let initialState: LangStateI = {
+	lng: "",
 }
+
+// first we check if we can use the language of the browser as the default
+// else, the default will be "en"
+const initialLang = navLang === "en" || navLang === "es" || navLang === "jp" ? navLang : "en"
+
+// then we check if there is any preferred_language stored in the device
+// else, will go the lang got from the validation before
+initialState.lng = localStorageLang ? localStorageLang : initialLang
 
 const langReducer = (state = initialState, action: LangAction) => {
 	switch (action.type) {
