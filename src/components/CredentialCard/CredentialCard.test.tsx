6344ba8c8be07ac8f83285811cb7af104d32fbe7
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-03-26 14:12:15 -0300

    fixed the test for credentialCard component

--- src/components/CredentialCard/CredentialCard.test.tsx
+++ src/components/CredentialCard/CredentialCard.test.tsx
@@ -4,16 +4,21 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import { faChevronRight, faPlusCircle } from "@fortawesome/free-solid-svg-icons"
 library.add(faChevronRight, faPlusCircle)
 
+import { Provider } from "react-redux"
+import store from "../../redux/store"
+
 import CredentialCard from "./index"
 
 import { credential4Testing, user4Testing } from "../../misc/Data4Testing"
 
 it("renders properly", () => {
 	const { getByTestId } = render(
-		<CredentialCard
-			credentials={credential4Testing}
-			availableSlots={user4Testing.availableSlots}
-		/>
+		<Provider store={store}>
+			<CredentialCard
+				credentials={credential4Testing}
+				availableSlots={user4Testing.slots_available}
+			/>
+		</Provider>
 	)
 
 	const credentialCard = getByTestId("test_credential_card_" + credential4Testing[0].id)
