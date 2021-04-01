a62f4468f61c17bd7eba9e104ff7468d555beae0
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-04-01 17:35:15 -0300

    created a basic model for the auth dialoge to unlock data

--- src/components/UnlockData/index.tsx
+++ src/components/UnlockData/index.tsx
@@ -1,6 +1,14 @@
 import react, { useState } from "react"
 
-import { IconButton, Tooltip } from "@material-ui/core"
+import {
+	IconButton,
+	Tooltip,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	Button,
+} from "@material-ui/core"
 
 import LockOpenIcon from "@material-ui/icons/LockOpen"
 import LockIcon from "@material-ui/icons/Lock"
@@ -18,14 +26,53 @@ type Props = {
 const UnlockData = ({ toggleLock, locked }: Props) => {
 	const { lng } = useSelector((state: RootState) => state.lng)
 
+	const [open, setOpen] = useState(false)
+
+	const toggleDialog = () => {
+		if (locked) {
+			setOpen(!open)
+		} else {
+			toggleLock()
+		}
+	}
+
+	const onAuthSuccess = () => {
+		setOpen(false)
+
+		toggleLock()
+	}
+
 	const tooltipTitle = translate("access_management", lng, locked ? 1 : 2)
 
 	return (
-		<Tooltip title={tooltipTitle} placement="right">
-			<IconButton color="primary" onClick={toggleLock} data-testid="test_toggle_lock">
-				{locked ? <LockIcon /> : <LockOpenIcon />}
-			</IconButton>
-		</Tooltip>
+		<>
+			<Tooltip title={tooltipTitle} placement="right">
+				<IconButton color="primary" onClick={toggleDialog} data-testid="test_toggle_lock">
+					{locked ? <LockIcon /> : <LockOpenIcon />}
+				</IconButton>
+			</Tooltip>
+
+			<Dialog
+				onClose={toggleDialog}
+				aria-labelledby="dialog-title"
+				open={open}
+				scroll="paper"
+				data-testid="test_dialog"
+			>
+				<DialogTitle id="dialog-title">titulo</DialogTitle>
+				<DialogContent>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum consectetur quas
+					in cumque consequuntur nesciunt molestiae ex eligendi sapiente saepe quidem,
+					reprehenderit ut aliquid, neque autem totam alias officia repellat?
+					<Button onClick={onAuthSuccess}>auth succeded</Button>
+				</DialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={toggleDialog} color="default" size="large">
+						{translate("go_back", lng)}
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
 	)
 }
 export default UnlockData
