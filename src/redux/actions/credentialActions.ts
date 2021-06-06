89ca68b91bcb19a51218025999580ae18db07af2
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-06 12:44:37 -0300

    finished the initializeCredential action, proceding to clean...

--- src/redux/actions/credentialActions.ts
+++ src/redux/actions/credentialActions.ts
@@ -37,35 +37,60 @@ export const initializeCredential = (credentialDB: CredentialT): EditCredentialI
 		initialC.description = credentialDB.description
 	}
 	if (credentialDB.email) {
+		/** */
+
 		asterisks = calcAsterisks(credentialDB.email.char_count)
 
 		initialC.email = credentialDB.email.ending + asterisks + credentialDB.email.ending
 	}
 	if (credentialDB.password) {
+		/** */
+
 		asterisks = calcAsterisks(credentialDB.password.char_count)
 
 		initialC.password = asterisks
 	}
 	if (credentialDB.phone_number) {
+		/** */
+
 		asterisks = calcAsterisks(credentialDB.phone_number.char_count)
 
 		initialC.phone_number =
 			credentialDB.phone_number.ending + asterisks + credentialDB.phone_number.ending
 	}
 	if (credentialDB.security_codes) {
+		/** */
+
 		if (credentialDB.security_codes.crypto_currency_access_code) {
+			/** */
+
+			initialC.crypto_currency_access_codes = createCodesArr(
+				credentialDB.security_codes.crypto_code_length
+			)
 		}
 		if (credentialDB.security_codes.multiple_security_codes) {
+			/** */
+
+			initialC.multiple_security_code = createCodesArr(
+				credentialDB.security_codes.multiple_code_length
+			)
 		}
 		if (credentialDB.security_codes.unique_security_code) {
+			/** */
+
 			initialC.unique_security_code = calcAsterisks(10)
 		}
 	}
 	if (credentialDB.security_question_answer) {
+		/** */
+
 		initialC.security_question = calcAsterisks(10)
+
 		initialC.security_answer = calcAsterisks(10)
 	}
 	if (credentialDB.user_name && credentialDB.char_count) {
+		/** */
+
 		asterisks = calcAsterisks(credentialDB.char_count)
 
 		initialC.user_name = asterisks
@@ -82,4 +107,8 @@ export const initializeCredential = (credentialDB: CredentialT): EditCredentialI
 
 const calcAsterisks = (charCount: number): string => {
 	return new Array(charCount).join("•")
+}
+
+const createCodesArr = (arrLength: number | null) => {
+	return Array.from(new Array(arrLength), () => "•••••")
 }
