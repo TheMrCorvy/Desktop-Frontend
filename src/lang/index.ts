e6f223a8356787036e49d24964142520fc990097
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-13 15:24:38 -0300

    added docs for translate function

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -2,6 +2,28 @@ import en from "./en.json"
 import es from "./es.json"
 import jp from "./jp.json"
 
+/**
+ * @alias Translate
+ *
+ * @module Translation
+ *
+ * @description This is the function that returns the text for all the components that use the translation feature.
+ *
+ * @param {string} key The key to access the JSON file
+ *
+ * @param {"es" | "en" | "jp"} language Tha language that is required
+ *
+ * @param {number} [index] The position on the array where is the needed text
+ *
+ * @example
+ *
+ * const translation = translate("hello_world", "es", 0)
+ *
+ * console.log("translation") // "Hola Mundo!"
+ *
+ * @returns The string stored on the given key
+ */
+
 export const translate = (key: string, language: string, index?: number) => {
 	let langData: { [key: string]: string[] } = {}
 
